9b377938b3993a44fedd59b51f767fb41b70e7f4
Jarmo Lindstrom <yaramodesu@gmail.com>
2020-12-15 14:50:19 +0200

    vähän vaan methodeja

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/shopApp', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
     console.log('Yhteys tietokantaan avattu')
 }).catch( err => {
     console.log('tuli pikku errori!')
     console.log(err)
 })
-
+/* 
 const movieSchema = new mongoose.Schema({
     title: String,
     year: Number,
@@ -14,7 +14,7 @@ const movieSchema = new mongoose.Schema({
     rating: String
 });
 
-const Movie = mongoose.model('Movie', movieSchema)
+const Movie = mongoose.model('Movie', movieSchema) */
 /* 
 const tahdonElokuva = new Movie({title: 'tahdon', year: 1987, score: 8, rating: 'r'})
  */
@@ -26,3 +26,86 @@ const tahdonElokuva = new Movie({title: 'tahdon', year: 1987, score: 8, rating: 
     console.log('toimii')
     console.log(data)
 }) */
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, 'Hinnan pitää olla positiivinen, tää on custom error viesti']
+    },
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    categories: [String],
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        inStore: {
+            type: Number,
+            default: 0
+        }
+    },
+    size: {
+        type: String,
+        // valitoidaan kokoja
+        enum: ['s', 'm', 'l']
+    }
+})
+
+//Muista käyttää vanhaa functota
+/* productSchema.methods.greet = function() {
+    console.log('hei vaan')
+    console.log(`-from: ${this.name}`)
+} */
+
+productSchema.methods.toggleOnSale = function() {
+    //muutetaan boolean vastakohdaksi
+    this.onSale = !this.onSale
+    return this.save()
+}
+
+productSchema.methods.addCategory = function(newCat) {
+    this.categories.push(newCat)
+    return this.save
+}
+
+const Product = mongoose.model('Product', productSchema)
+
+
+ const findProduct = async() => {
+     const foundProduct = await Product.findOne({name: 'gloves'})
+     console.log(foundProduct)
+     await foundProduct.toggleOnSale()
+     await foundProduct.addCategory('ulkoilu')
+     console.log(foundProduct)
+
+ }
+
+
+
+ findProduct();
+/* const bike = new Product({ name: 'gloves', price: 9, categories: ['cycling']})
+bike.save()
+.then(data => {
+    console.log('se toimii')
+    console.log(data)
+})
+.catch(err => {
+    console.log('errooor')
+    console.log(err)
+}) */
+Product.findOneAndUpdate({name: 'gloves'}, {price: 10}, {new: true, runValidators: true})
+.then(data => {
+    console.log('se toimii')
+    console.log(data)
+})
+.catch(err => {
+    console.log('errooor')
+    console.log(err)})
